amscanne/bpftrace

refactor: drop `is_literal` and add folding pass
In order to reduce the complexity inside the more complex passes (the
semantic analyser and code generation), add a folding pass for literals
and constants. Note that this is not intended to do the work that LLVM
will do in the backend, rather it is merely folding constants that must
be interpreted by semantic analysis.

A classic example is `str($1)`, which can be folded into a suitable
`String` node. This eliminates the need for special cases in semantic
analysis and the `is_in_str` field within the AST.

Signed-off-by: Adin Scannell <amscanne@meta.com>

stack-info: PR: https://github.com/bpftrace/bpftrace/pull/3984, branch: user/amscanne/map_pipelines/7

src/ast/ast.cpp
@@ -11,16 +11,19 @@ namespace bpftrace::ast {
 
 static constexpr std::string_view ENUM = "enum ";
 
-Integer::Integer(Diagnostics &d, int64_t n, Location &&loc, bool is_negative)
-    : Expression(d, std::move(loc)), n(n), is_negative(is_negative)
+Integer::Integer(Diagnostics &d, int64_t n, Location &&loc)
+    : Expression(d, std::move(loc)), value(n)
 {
-  is_literal = true;
+}
+
+NegativeInteger::NegativeInteger(Diagnostics &d, int64_t n, Location &&loc)
+    : Expression(d, std::move(loc)), value(n)
+{
 }
 
 String::String(Diagnostics &d, std::string str, Location &&loc)
-    : Expression(d, std::move(loc)), str(std::move(str))
+    : Expression(d, std::move(loc)), value(std::move(str))
 {
-  is_literal = true;
 }
 
 Builtin::Builtin(Diagnostics &d, std::string ident, Location &&loc)
@@ -36,14 +39,12 @@ Identifier::Identifier(Diagnostics &d, std::string ident, Location &&loc)
 PositionalParameter::PositionalParameter(Diagnostics &d, long n, Location &&loc)
     : Expression(d, std::move(loc)), n(n)
 {
-  is_literal = true;
 }
 
 PositionalParameterCount::PositionalParameterCount(Diagnostics &d,
                                                    Location &&loc)
     : Expression(d, std::move(loc))
 {
-  is_literal = true;
 }
 
 Call::Call(Diagnostics &d, std::string func, Location &&loc)

src/ast/ast.h
@@ -113,27 +113,25 @@ public:
   Map *key_for_map = nullptr;
   Map *map = nullptr;      // Only set when this expression is assigned to a map
   Variable *var = nullptr; // Set when this expression is assigned to a variable
-  bool is_literal = false;
 };
 using ExpressionList = std::vector<Expression *>;
 
 class Integer : public Expression {
 public:
-  explicit Integer(Diagnostics &d,
-                   int64_t n,
-                   Location &&loc,
-                   bool is_negative = true);
+  explicit Integer(Diagnostics &d, int64_t n, Location &&loc);
+  const uint64_t value;
+};
 
-  int64_t n;
-  bool is_negative;
+class NegativeInteger : public Expression {
+public:
+  explicit NegativeInteger(Diagnostics &d, int64_t n, Location &&loc);
+  const int64_t value;
 };
 
 class PositionalParameter : public Expression {
 public:
   explicit PositionalParameter(Diagnostics &d, long n, Location &&loc);
-
-  long n;
-  bool is_in_str = false;
+  const long n;
 };
 
 class PositionalParameterCount : public Expression {
@@ -144,8 +142,7 @@ public:
 class String : public Expression {
 public:
   explicit String(Diagnostics &d, std::string str, Location &&loc);
-
-  std::string str;
+  const std::string value;
 };
 
 class Identifier : public Expression {
@@ -417,7 +414,6 @@ class Unroll : public Statement {
 public:
   Unroll(Diagnostics &d, Expression *expr, Block *block, Location &&loc);
 
-  long int var = 0;
   Expression *expr = nullptr;
   Block *block = nullptr;
 };

yangyyyy15/mybpftrace

src/ast/ast.cpp
@@ -20,16 +20,19 @@ Diagnostic &Node::addWarning() const
 
 static constexpr std::string_view ENUM = "enum ";
 
-Integer::Integer(ASTContext &ctx, int64_t n, Location &&loc, bool is_negative)
-    : Expression(ctx, std::move(loc)), n(n), is_negative(is_negative)
+Integer::Integer(ASTContext &ctx, int64_t n, Location &&loc)
+    : Expression(ctx, std::move(loc)), value(n)
 {
-  is_literal = true;
+}
+
+NegativeInteger::NegativeInteger(ASTContext &ctx, int64_t n, Location &&loc)
+    : Expression(ctx, std::move(loc)), value(n)
+{
 }
 
 String::String(ASTContext &ctx, std::string str, Location &&loc)
-    : Expression(ctx, std::move(loc)), str(std::move(str))
+    : Expression(ctx, std::move(loc)), value(std::move(str))
 {
-  is_literal = true;
 }
 
 Builtin::Builtin(ASTContext &ctx, std::string ident, Location &&loc)
@@ -47,14 +50,12 @@ PositionalParameter::PositionalParameter(ASTContext &ctx,
                                          Location &&loc)
     : Expression(ctx, std::move(loc)), n(n)
 {
-  is_literal = true;
 }
 
 PositionalParameterCount::PositionalParameterCount(ASTContext &ctx,
                                                    Location &&loc)
     : Expression(ctx, std::move(loc))
 {
-  is_literal = true;
 }
 
 Call::Call(ASTContext &ctx, std::string func, Location &&loc)

src/ast/ast.h
@@ -97,27 +97,25 @@ public:
   Map *key_for_map = nullptr;
   Map *map = nullptr;      // Only set when this expression is assigned to a map
   Variable *var = nullptr; // Set when this expression is assigned to a variable
-  bool is_literal = false;
 };
 using ExpressionList = std::vector<Expression *>;
 
 class Integer : public Expression {
 public:
-  explicit Integer(ASTContext &ctx,
-                   int64_t n,
-                   Location &&loc,
-                   bool is_negative = true);
+  explicit Integer(ASTContext &ctx, int64_t n, Location &&loc);
+  const uint64_t value;
+};
 
-  int64_t n;
-  bool is_negative;
+class NegativeInteger : public Expression {
+public:
+  explicit NegativeInteger(ASTContext &ctx, int64_t n, Location &&loc);
+  const int64_t value;
 };
 
 class PositionalParameter : public Expression {
 public:
   explicit PositionalParameter(ASTContext &ctx, long n, Location &&loc);
-
-  long n;
-  bool is_in_str = false;
+  const long n;
 };
 
 class PositionalParameterCount : public Expression {
@@ -128,8 +126,7 @@ public:
 class String : public Expression {
 public:
   explicit String(ASTContext &ctx, std::string str, Location &&loc);
-
-  std::string str;
+  const std::string value;
 };
 
 class Identifier : public Expression {
@@ -399,7 +396,6 @@ class Unroll : public Statement {
 public:
   Unroll(ASTContext &ctx, Expression *expr, Block *block, Location &&loc);
 
-  long int var = 0;
   Expression *expr = nullptr;
   Block *block = nullptr;
 };