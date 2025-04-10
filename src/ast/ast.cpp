amscanne/bpftrace

ast: Implement map "de-sugaring" as a separate pass
In order to simplify and standardize code generation, implement an early
pass that desugars map syntax, and starts to handle code paths are
currently pushed back to code generation.

There are three major things done here, which are somewhat entangled.

1) Always fill in default keys.

Currently, code generation will transparently fill in the keys for
scalar maps, e.g. `@x` becomes an array map of size 1.

Since this will be automatically recognized as a scalar map, we simply
desugar this statement by doing `@x[0]` for that first case. All error
messages will be unified aroud paths where maps diverge for any reason,
e.g. `@x = 1; @x["foo"] = 1`.

2) Split map accesses into explicit reads `MapAccess` and writes
   `AssignMapStatement`. Both of these require explicit keys.

(Note that in the future we could make assignments an expression also,
but this should come after fixing up some of the code generation
semantics to make this a bit cleaner.)

This removes a lot of the special cases in the semantic analyzer (e.g.
`skip_map_key_validation`, `is_read`) because these are simply different
node in the tree explicitly.

3) Turn `@x[0] = avg()` into `avg(@x, 0)`.

The only way to avoid the map `@x` being desugared into `@x[0]` is to
use it in the context of a special assignment. These will be split into
the map argument and the key expression, and passed to the function.

The semantic analyser is able to infer the map type from the hard-coded
function prototypes for these special functions. In the future, we can
extend this inference to non-hard-coded functions.

This has the biggest effect on internals, and allows us to drop the
inverted pointers (e.g. `Expression->map`), since code generation will
have all the context required to emit the necessary code.

Signed-off-by: Adin Scannell <amscanne@meta.com>

stack-info: PR: https://github.com/bpftrace/bpftrace/pull/3988, branch: user/amscanne/map_pipelines/8

src/ast/ast.cpp
@@ -100,15 +100,9 @@ MapDeclStatement::MapDeclStatement(Diagnostics &d,
 {
 }
 
-Map::Map(Diagnostics &d,
-         std::string ident,
-         Expression *key_expr,
-         Location &&loc)
-    : Expression(d, std::move(loc)), ident(std::move(ident)), key_expr(key_expr)
+Map::Map(Diagnostics &d, std::string ident, Location &&loc)
+    : Expression(d, std::move(loc)), ident(std::move(ident))
 {
-  if (key_expr) {
-    key_expr->key_for_map = this;
-  }
 }
 
 Variable::Variable(Diagnostics &d, std::string ident, Location &&loc)
@@ -167,6 +161,11 @@ TupleAccess::TupleAccess(Diagnostics &d,
 {
 }
 
+MapAccess::MapAccess(Diagnostics &d, Map *map, Expression *key, Location &&loc)
+    : Expression(d, std::move(loc)), map(map), key(key)
+{
+}
+
 Cast::Cast(Diagnostics &d,
            SizedType cast_type,
            Expression *expr,
@@ -186,26 +185,20 @@ ExprStatement::ExprStatement(Diagnostics &d, Expression *expr, Location &&loc)
 {
 }
 
+AssignScalarMapStatement::AssignScalarMapStatement(Diagnostics &d,
+                                                   Map *map,
+                                                   Expression *expr,
+                                                   Location &&loc)
+    : Statement(d, std::move(loc)), map(map), expr(expr) {};
+
 AssignMapStatement::AssignMapStatement(Diagnostics &d,
                                        Map *map,
+                                       Expression *key,
                                        Expression *expr,
                                        Location &&loc)
-    : Statement(d, std::move(loc)), map(map), expr(expr)
+    : Statement(d, std::move(loc)), map(map), key(key), expr(expr)
 {
-  // If this is a block expression, then we skip through that and actually set
-  // the map on the underlying expression. This is done recursively. It is only
-  // done to support functions that need to know the type of the map to which
-  // they are being assigned.
-  Expression *value = expr;
-  while (true) {
-    auto *block = dynamic_cast<Block *>(value);
-    if (block == nullptr) {
-      break;
-    }
-    value = block->expr; // Must be non-null if expression.
-  };
-  value->map = map;
-};
+}
 
 AssignVarStatement::AssignVarStatement(Diagnostics &d,
                                        Variable *var,
@@ -213,7 +206,6 @@ AssignVarStatement::AssignVarStatement(Diagnostics &d,
                                        Location &&loc)
     : Statement(d, std::move(loc)), var(var), expr(expr)
 {
-  expr->var = var;
 }
 
 AssignVarStatement::AssignVarStatement(Diagnostics &d,
@@ -225,7 +217,6 @@ AssignVarStatement::AssignVarStatement(Diagnostics &d,
       var(var_decl_stmt->var),
       expr(expr)
 {
-  expr->var = var;
 }
 
 AssignConfigVarStatement::AssignConfigVarStatement(Diagnostics &d,

src/ast/ast.h
@@ -110,9 +110,6 @@ public:
   ~Expression() override = default;
 
   SizedType type;
-  Map *key_for_map = nullptr;
-  Map *map = nullptr;      // Only set when this expression is assigned to a map
-  Variable *var = nullptr; // Set when this expression is assigned to a variable
 };
 using ExpressionList = std::vector<Expression *>;
 
@@ -177,6 +174,13 @@ public:
 
   std::string func;
   ExpressionList vargs;
+
+  // Some passes may inject new arguments to the call, which is always
+  // done at the beginning (in order to support variadic arguments) for
+  // later passes. This is a result of "desugaring" some syntax. When this
+  // happens, this number is increased so that later error reporting can
+  // correctly account for this.
+  size_t injected_args = 0;
 };
 
 class Sizeof : public Expression {
@@ -218,19 +222,10 @@ public:
 
 class Map : public Expression {
 public:
-  explicit Map(Diagnostics &d,
-               std::string ident,
-               Expression *expr,
-               Location &&loc);
+  explicit Map(Diagnostics &d, std::string ident, Location &&loc);
 
   std::string ident;
-  Expression *key_expr = nullptr;
   SizedType key_type;
-  bool skip_key_validation = false;
-  // This is for a feature check on reading per-cpu maps
-  // which involve calling map_lookup_percpu_elem
-  // https://github.com/bpftrace/bpftrace/issues/3755
-  bool is_read = true;
 };
 
 class Variable : public Expression {
@@ -296,6 +291,14 @@ public:
   ssize_t index;
 };
 
+class MapAccess : public Expression {
+public:
+  MapAccess(Diagnostics &d, Map *map, Expression *key, Location &&loc);
+
+  Map *map = nullptr;
+  Expression *key = nullptr;
+};
+
 class Cast : public Expression {
 public:
   Cast(Diagnostics &d, SizedType type, Expression *expr, Location &&loc);
@@ -338,14 +341,32 @@ public:
   bool set_type = false;
 };
 
+// Scalar map assignment is purely a syntax sugar that is removed by the pass
+// returned by`CreateMapSugarPass`. This is replaced by the expansion of
+// default keys, so that later steps (semantic analysis, code generation),
+// don't need to worry about this. Maps whose accesses are bound to a single
+// key will be automatically collapsed into scalar values on the output side.
+class AssignScalarMapStatement : public Statement {
+public:
+  AssignScalarMapStatement(Diagnostics &d,
+                           Map *map,
+                           Expression *expr,
+                           Location &&loc);
+
+  Map *map = nullptr;
+  Expression *expr = nullptr;
+};
+
 class AssignMapStatement : public Statement {
 public:
   AssignMapStatement(Diagnostics &d,
                      Map *map,
+                     Expression *key,
                      Expression *expr,
                      Location &&loc);
 
   Map *map = nullptr;
+  Expression *key = nullptr;
   Expression *expr = nullptr;
 };
 

yangyyyy15/mybpftrace

src/ast/ast.cpp
@@ -111,17 +111,11 @@ MapDeclStatement::MapDeclStatement(ASTContext &ctx,
 {
 }
 
-Map::Map(ASTContext &ctx,
-         std::string ident,
-         Expression *key_expr,
-         Location &&loc)
+Map::Map(ASTContext &ctx, std::string ident, Location &&loc)
     : Expression(ctx, std::move(loc)),
       ident(std::move(ident)),
       key_expr(key_expr)
 {
-  if (key_expr) {
-    key_expr->key_for_map = this;
-  }
 }
 
 Variable::Variable(ASTContext &ctx, std::string ident, Location &&loc)
@@ -183,6 +177,11 @@ TupleAccess::TupleAccess(ASTContext &ctx,
 {
 }
 
+MapAccess::MapAccess(ASTContext &ctx, Map *map, Expression *key, Location &&loc)
+    : Expression(ctx, std::move(loc)), map(map), key(key)
+{
+}
+
 Cast::Cast(ASTContext &ctx,
            SizedType cast_type,
            Expression *expr,
@@ -202,26 +201,20 @@ ExprStatement::ExprStatement(ASTContext &ctx, Expression *expr, Location &&loc)
 {
 }
 
+AssignScalarMapStatement::AssignScalarMapStatement(ASTContext &ctx,
+                                                   Map *map,
+                                                   Expression *expr,
+                                                   Location &&loc)
+    : Statement(ctx, std::move(loc)), map(map), expr(expr) {};
+
 AssignMapStatement::AssignMapStatement(ASTContext &ctx,
                                        Map *map,
+                                       Expression *key,
                                        Expression *expr,
                                        Location &&loc)
-    : Statement(ctx, std::move(loc)), map(map), expr(expr)
+    : Statement(ctx, std::move(loc)), map(map), key(key), expr(expr)
 {
-  // If this is a block expression, then we skip through that and actually set
-  // the map on the underlying expression. This is done recursively. It is only
-  // done to support functions that need to know the type of the map to which
-  // they are being assigned.
-  Expression *value = expr;
-  while (true) {
-    auto *block = dynamic_cast<Block *>(value);
-    if (block == nullptr) {
-      break;
-    }
-    value = block->expr; // Must be non-null if expression.
-  };
-  value->map = map;
-};
+}
 
 AssignVarStatement::AssignVarStatement(ASTContext &ctx,
                                        Variable *var,
@@ -229,7 +222,6 @@ AssignVarStatement::AssignVarStatement(ASTContext &ctx,
                                        Location &&loc)
     : Statement(ctx, std::move(loc)), var(var), expr(expr)
 {
-  expr->var = var;
 }
 
 AssignVarStatement::AssignVarStatement(ASTContext &ctx,
@@ -241,7 +233,6 @@ AssignVarStatement::AssignVarStatement(ASTContext &ctx,
       var(var_decl_stmt->var),
       expr(expr)
 {
-  expr->var = var;
 }
 
 AssignConfigVarStatement::AssignConfigVarStatement(ASTContext &ctx,

src/ast/ast.h
@@ -94,9 +94,6 @@ public:
   ~Expression() override = default;
 
   SizedType type;
-  Map *key_for_map = nullptr;
-  Map *map = nullptr;      // Only set when this expression is assigned to a map
-  Variable *var = nullptr; // Set when this expression is assigned to a variable
 };
 using ExpressionList = std::vector<Expression *>;
 
@@ -161,6 +158,13 @@ public:
 
   std::string func;
   ExpressionList vargs;
+
+  // Some passes may inject new arguments to the call, which is always
+  // done at the beginning (in order to support variadic arguments) for
+  // later passes. This is a result of "desugaring" some syntax. When this
+  // happens, this number is increased so that later error reporting can
+  // correctly account for this.
+  size_t injected_args = 0;
 };
 
 class Sizeof : public Expression {
@@ -202,19 +206,10 @@ public:
 
 class Map : public Expression {
 public:
-  explicit Map(ASTContext &ctx,
-               std::string ident,
-               Expression *expr,
-               Location &&loc);
+  explicit Map(ASTContext &ctx, std::string ident, Location &&loc);
 
   std::string ident;
-  Expression *key_expr = nullptr;
   SizedType key_type;
-  bool skip_key_validation = false;
-  // This is for a feature check on reading per-cpu maps
-  // which involve calling map_lookup_percpu_elem
-  // https://github.com/bpftrace/bpftrace/issues/3755
-  bool is_read = true;
 };
 
 class Variable : public Expression {
@@ -280,6 +275,14 @@ public:
   ssize_t index;
 };
 
+class MapAccess : public Expression {
+public:
+  MapAccess(ASTContext &ctx, Map *map, Expression *key, Location &&loc);
+
+  Map *map = nullptr;
+  Expression *key = nullptr;
+};
+
 class Cast : public Expression {
 public:
   Cast(ASTContext &ctx, SizedType type, Expression *expr, Location &&loc);
@@ -322,14 +325,32 @@ public:
   bool set_type = false;
 };
 
+// Scalar map assignment is purely a syntax sugar that is removed by the pass
+// returned by`CreateMapSugarPass`. This is replaced by the expansion of
+// default keys, so that later steps (semantic analysis, code generation),
+// don't need to worry about this. Maps whose accesses are bound to a single
+// key will be automatically collapsed into scalar values on the output side.
+class AssignScalarMapStatement : public Statement {
+public:
+  AssignScalarMapStatement(ASTContext &ctx,
+                           Map *map,
+                           Expression *expr,
+                           Location &&loc);
+
+  Map *map = nullptr;
+  Expression *expr = nullptr;
+};
+
 class AssignMapStatement : public Statement {
 public:
   AssignMapStatement(ASTContext &ctx,
                      Map *map,
+                     Expression *key,
                      Expression *expr,
                      Location &&loc);
 
   Map *map = nullptr;
+  Expression *key = nullptr;
   Expression *expr = nullptr;
 };
 